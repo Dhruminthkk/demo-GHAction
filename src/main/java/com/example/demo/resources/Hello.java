bc48906d4d39519335f250bc785a1418ff7fa453
Dhrumin Thakkar <dhruminthkkr@gmail.com>
2021-10-17 14:44:03 +0530

    feat: :sparkles: added demo project for spring boot

---
+++ src/main/java/com/example/demo/resources/Hello.java
@@ -0,0 +1,25 @@
+package com.example.demo.resources;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/docker/demo")
+public class Hello {
+	
+	@GetMapping
+	public String hello() {
+		return "Hello from App deployed by docker version == 2";
+	}
+	
+	
+}
